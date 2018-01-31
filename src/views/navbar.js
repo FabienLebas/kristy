fedacee70887c811187d712510e51d8e63cc3db3
Fabien Lebas <fabien.lebas@decathlon.com>
2018-01-31 09:33:43 +0100

    Navbar layout

--- src/views/navbar.js
+++ src/views/navbar.js
@@ -8,19 +8,15 @@ export default class DisplayNavBar extends Component{
   render(){
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        
-        <Link className="navbar-brand" to={"/"}>Kristy Shop</Link>
+        <Link className="navbar-brand" to={"/"}><h2>KRISTY SHOP</h2></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <div className="collapse navbar-collapse offset-9" id="navbarNavDropdown">
           <ul className="navbar-nav">
-
             <li className="nav-item">
-
-              <Link className="nav-link" to={"/cart"}><i className="fa fa-shopping-cart" aria-hidden="true"></i></Link>
+              <Link className="nav-link" to={"/cart"}><i className="fa fa-shopping-cart fa-3x" aria-hidden="true"></i></Link>
             </li>
-
           </ul>
         </div>
       </nav>
