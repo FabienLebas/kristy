d3b5f4ac356f4fbf5de8c24cfe6835a388ca7aec
Fabien Lebas <fabien.lebas@decathlon.com>
2018-02-01 11:49:46 +0100

    stripe wip

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import displayCartContent from '../modules/cart/displayCartContent';
 import {removeFromCart, addToCart } from '../modules/cart/addToCart';
-
-
-
+import StripeCheckout from "react-stripe-checkout";
 
 export default class Cart extends Component {
+
+  onToken = token => {
+    fetch("/charge", {
+      method: "POST",
+      body: JSON.stringify({
+        stripeData: token,
+        products: [
+          {id: 42, quantity: 2},
+          {id: 1337, quantity: 1}
+        ]
+      }),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === "succeeded") {
+          console.log(data);
+          // dispatch a success
+        } else {
+          console.warn(data);
+          // dispatch an error
+        }
+      });
+  };
 
 displayItem(element, qty){
   return (
@@ -48,27 +70,30 @@ displayItem(element, qty){
                  </tbody>
           </table>
         </div>
+
+        <div className="App-intro">
+          <StripeCheckout
+            token={this.onToken}
+            amount={999}
+            currency="EUR"
+            stripeKey={"pk_test_0p6Op2nc9ozHT1Ojdz1NuqeK"}
+          />
+        </div>
+        <div className="App-intro">
+          <StripeCheckout
+            token={this.onToken}
+            amount={999}
+            currency="EUR"
+            stripeKey={"pk_test_0p6Op2nc9ozHT1Ojdz1NuqeK"}
+            email="toto@toto.com"
+            name="My Demo of Stripe"
+            description="Change me into a description"
+          >
+            <button className="btn btn-primary">Pay with a custom button</button>
+          </StripeCheckout>
+        </div>
       </div>
 
     );
   }
 }
-
-
-
-
-
-  // <div>
-  //   <table className= "table">
-  //     <thead>
-  //        <tr>
-  //          <th scope="col"></th>
-  //          <th scope="col">Article</th>
-  //          <th scope="col">Quantity</th>
-  //          <th scope="col">price</th>
-  //          <th scope="col">Delete</th>
-  //        </tr>
-  //      </thead>
-
-  //   </table>
-  // </div>
